66bca65edad1d7d55d716f1341b7e82ad666fbb6
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-26 17:01:18 -0500

    Animated and styled resume / website links under intro

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,7 +1,10 @@
 "use client"
-
+import { BsArrowRight, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { FaGithubSquare } from 'react-icons/fa';
 
 const Intro = () => {
   return (
@@ -51,6 +54,26 @@ const Intro = () => {
         <span className="font-bold">Let&apos;s Connect!</span>
 
       </motion.p>
+
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100}}
+        animate={{ opacity: 1, y: 0}}
+        transition={{delay: 0.1}}
+      >
+        <Link href='#contact'
+          className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2
+          rounded-full'
+        >Contact me <BsArrowRight/>{' '}</Link>
+        <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+          Download Resume <HiDownload />
+        </a>
+        <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+          <BsLinkedin />
+        </a>
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]'>
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   )
 }
