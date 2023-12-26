26091125eff3b7c04139295249eef9546c24fc1f
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-26 17:25:55 -0500

    Added more animations and link styles and fixed broken links

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -58,19 +58,37 @@ const Intro = () => {
       <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
         initial={{ opacity: 0, y: 100}}
         animate={{ opacity: 1, y: 0}}
-        transition={{delay: 0.1}}
+        transition={{delay: 0.1 }}
       >
         <Link href='#contact'
-          className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2
-          rounded-full'
-        >Contact me <BsArrowRight/>{' '}</Link>
-        <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
-          Download Resume <HiDownload />
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2
+          rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950
+          active:scale-105 transition'
+        >
+          Contact me <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>{' '}
+        </Link>
+
+        <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full 
+        outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer
+        border border-black/10'
+          href='/resume.pdf' 
+          download
+        >
+          Download Resume <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
         </a>
-        <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
-          <BsLinkedin />
+
+        <a className='group bg-white p-4 flex items-center gap-2 rounded-full 
+        outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer
+        border border-black/10'
+          href='https://www.linkedin.com/in/aaron-baumgarten-8492b215b' target='_blank'
+        >
+          <BsLinkedin className='opacity-70'/>
         </a>
-        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]'>
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]
+        outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer
+        border border-black/10'
+          href='https://github.com/aaronbau1' target='_blank'
+        >
           <FaGithubSquare />
         </a>
       </motion.div>
