2f6874789a6bd4313e105150f9c0b33befed8765
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-27 14:57:28 -0500

    Created boilerplate projects component

---
+++ src/app/components/Projects.tsx
@@ -0,0 +1,11 @@
+import SectionHeading from "./Section-Heading"
+
+const Projects = () => {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+    </section>
+  )
+}
+
+export default Projects
