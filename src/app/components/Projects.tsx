2df2ebb2404c2e4cc57ba2e2839c7e90141cc369
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-27 18:47:34 -0500

    Added images to project cards, need to style cards next

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { projectsData } from "../../../lib/data"
 import SectionHeading from "./Section-Heading"
+import Image from "next/image"
 
 const Projects = () => {
   return (
@@ -29,6 +30,16 @@ function Project({
   tags,
   imageUrl }: ProjectProps) {
   return (
-    <div>{title}</div>
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag,index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+
+      <Image src={imageUrl} alt={title} quality={95}/>
+    </section>
   )
 }
