deb69ef78fc853acc91aa06a953f48ff46ac9c9d
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-27 15:18:27 -0500

    Added Project card component and mapping

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,11 +1,34 @@
+import React from "react"
+import { projectsData } from "../../../lib/data"
 import SectionHeading from "./Section-Heading"
 
 const Projects = () => {
   return (
     <section>
       <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {
+          projectsData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project}/>
+            </React.Fragment>
+          ))
+        }
+      </div>
     </section>
   )
 }
 
-export default Projects
+export default Projects;
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ 
+  title,
+  description,
+  tags,
+  imageUrl }: ProjectProps) {
+  return (
+    <div>{title}</div>
+  )
+}
