4d9da443d0e811d01c94f98a7c3db4d2ecd9bb21
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-28 10:52:05 -0500

    Added hover effects and hover animations to project card

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -32,7 +32,7 @@ function Project({
   return (
     <section className="group bg-gray-100 max-w-[42rem] border border-black/5
      overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0
-     even:pl-8">
+     even:pl-8 hover:bg-gray-200 transition">
       <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 
       sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold">
@@ -54,8 +54,20 @@ function Project({
       </div>
         
       <Image src={imageUrl} alt={title} quality={95} 
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        group-even:right-[initial] group-even:-left-40"
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition
+        group-even:right-[initial] 
+        group-even:-left-40
+        
+        group-hover:scale-[1.04]
+        group-hover:-translate-x-3
+        group-hover:-translate-y-3
+        group-hover:-rotate-2
+
+        group-even:group-hover:scale-[1.04]
+        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3
+        group-even:group-hover:rotate-2
+        "
       />
   </section>
   )
