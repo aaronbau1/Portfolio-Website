49f2405ddb3f3e57fa2beb0dc8e0a6457a521f17
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-28 20:26:58 -0500

    Added hover styling and active section tracking to Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,8 +3,13 @@
 import { motion } from 'framer-motion';
 import { links } from '../../../lib/data';
 import Link from 'next/link';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 const Header = () => {
+
+  const[activeSection, setActiveSection] = useState('Home');
+
   return (
     <header className="z-[100] relative">
       <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none
@@ -27,10 +32,32 @@ const Header = () => {
               animate={{y: 0, opacity: 1}}
             >
               <Link
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
+                  {
+                  "text-gray-950": activeSection === link.name
+                  }
+                )}
                 href={link.hash}
+                onClick={() => setActiveSection(link.name)}
               >
                 {link.name}
+                
+                {
+                  link.name === activeSection ? (
+                    <motion.span className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+                      layoutId='activeSection'
+                      transition={{
+                        type: "spring",
+                        stiffness: 380,
+                        damping: 30,
+                      }}
+                    >
+
+                    </motion.span>
+                  ) : null
+                }
+
+                
               </Link>
             </motion.li>
           ))}
