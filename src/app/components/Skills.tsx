676b5323135f71efd59ed5cec7f92d67d995c046
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-30 11:03:31 -0500

    Added Skills section template

---
+++ src/app/components/Skills.tsx
@@ -0,0 +1,11 @@
+import SectionHeading from "./Section-Heading";
+
+const Skills = () => {
+  return (
+    <section>
+      <SectionHeading>My Skills</SectionHeading>
+    </section>
+  )
+}
+
+export default Skills;
