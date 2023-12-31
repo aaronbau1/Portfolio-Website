f3e7a7c854047c177ad161434592b16fd6eefac3
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-31 16:12:05 -0500

    Added skeleton for Experience section

---
+++ src/app/components/Experience.tsx
@@ -0,0 +1,11 @@
+import SectionHeading from "./Section-Heading"
+
+const Experience = () => {
+  return (
+    <section id='experience'>
+      <SectionHeading>My Experience</SectionHeading>
+    </section>
+  )
+}
+
+export default Experience
