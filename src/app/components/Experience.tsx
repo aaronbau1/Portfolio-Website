d1b3ed5f56a8189a539aa661fcfb128794a4dbf5
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-31 16:39:03 -0500

    Implemented basic timeline component to experience section, need to style and debug

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,9 +1,31 @@
+"use client"
+
+import { experiencesData } from "@/lib/data";
 import SectionHeading from "./Section-Heading"
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import React from "react";
+import { useSectionInView } from "@/lib/hooks";
 
 const Experience = () => {
+
+  const { ref } = useSectionInView('Experience');
+
   return (
-    <section id='experience'>
+    <section ref={ref} id='experience'>
       <SectionHeading>My Experience</SectionHeading>
+      <VerticalTimeline>
+        {
+          experiencesData.map((item, index) => (
+            <React.Fragment key={index}>
+              <VerticalTimelineElement>
+                <h3>{item.title}</h3>
+                <p>{item.location}</p>
+                <p>{item.description}</p>
+              </VerticalTimelineElement>
+            </React.Fragment>
+          ))}
+      </VerticalTimeline>
     </section>
   )
 }
