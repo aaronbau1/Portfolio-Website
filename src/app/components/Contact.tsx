85a108d08a2c67fd88e4aa829f512eedf58c8dc7
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-01 22:37:59 -0500

    Styled Contact section

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -3,18 +3,23 @@ import SectionHeading from "./Section-Heading"
 
 const Contact = () => {
   return (
-    <section id="contact">
+    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
       <SectionHeading>Contact Me</SectionHeading>
-      <p>
+      <p className="text-gray-700 -mt-6">
         Please contact me directly at{' '} 
         <a className='underline' href='mailto:aaronbau@umich.edu'>aaronbau@umich.edu</a> or through this form
       </p>
 
-      <form>
-        <input type='email' />
-        <textarea />
-        <button type='submit'>
-          Submit <FaPaperPlane />
+      <form className="mt-10 flex flex-col">
+        <input type='email' className="h-14 rounded-lg borderBlack px-4" placeholder="Your email address"/>
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Your message"/>
+        <button type='submit' className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white 
+          rounded-full outline-none transition-all hover:bg-gray-950 focus:scale-110 hover:scale-110 active:scale-105">
+          Submit{' '}
+          <FaPaperPlane className='text-xs opacity-70 transition-all
+            group-hover:translate-x-1
+            group-hover:-translate-y-1'
+          />
         </button>
       </form>
     </section>
