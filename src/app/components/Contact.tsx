4bb2849cba91c2eb6cec35a10da44c3c58e733b2
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-01 19:19:18 -0500

    Added Contact Me skeleton for section

---
+++ src/app/components/Contact.tsx
@@ -0,0 +1,15 @@
+import SectionHeading from "./Section-Heading"
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <SectionHeading>Contact Me</SectionHeading>
+      <p>
+        Please contact me directly at 
+        <a className='underline' href=''>aaronbau@umich.edu</a> or through this form
+      </p>
+    </section>
+  )
+}
+
+export default Contact
