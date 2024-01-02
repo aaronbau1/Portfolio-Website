59513ec63c30ab821835aa9ca713fc19c291d4e3
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-01 23:25:34 -0500

    Updated link in Intro to scroll to Contact section

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -13,6 +13,7 @@ import { useSectionInView } from '@/lib/hooks';
 const Intro = () => {
 
   const { ref } = useSectionInView('Home', 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
@@ -71,6 +72,10 @@ const Intro = () => {
           className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2
           rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950
           active:scale-105 transition'
+          onClick={() => {
+            setActiveSection('Contact');
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>{' '}
         </Link>
