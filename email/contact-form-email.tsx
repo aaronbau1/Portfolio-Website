cdb00f10c0363febef2feb3c078ad68305e47a3b
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-03 15:12:06 -0500

    Styled custom email template from Contact section

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -22,10 +22,12 @@ const ContactFormEmail = ({message, senderEmail}: ContactFormEmailProps) => {
       <Head />
       <Preview>New message from your portfolio site</Preview>
       <Tailwind>
-        <Body>
+        <Body className='bg-gray-100 text-black'>
           <Container>
-            <Section>
-              <Heading>New message from Portfolio Website contact form</Heading>
+            <Section className='bg-white borderBlack my-10 px-10 py-4 rounded-md'>
+              <Heading className='leading-tight'>
+                New message from Portfolio Website contact form
+              </Heading>
               <Text>{message}</Text>
               <Hr />
               <Text>The sender's email is: {senderEmail}</Text>
