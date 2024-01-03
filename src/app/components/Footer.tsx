f88ebcdd305f70494012312f6d644921515f9a45
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-03 18:29:20 -0500

    Created skeleton for Footer section

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer>
+      <small>
+        &copy; 2024 Baumgarten. All rights reserved.
+      </small>
+      <p>
+      {/* <span>This website was</span> */}
+        <span>This website was</span> built with React, Next.js,
+        TypeScript, Tailwind CSS, Framer Motion, React Email & Resend. 
+        Hosted on Vercel.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
