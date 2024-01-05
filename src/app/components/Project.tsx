fd1ccbde9b665cbb4ecede2dd262b50a19b564d9
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-04 20:46:55 -0500

    Updated Project animations for  mobile devices

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -61,10 +61,14 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
           group-hover:-translate-y-3
           group-hover:-rotate-2
 
-          group-even:group-hover:scale-[1.04]
-          group-even:group-hover:translate-x-3
-          group-even:group-hover:translate-y-3
-          group-even:group-hover:rotate-2"
+          sm:group-even:group-hover:scale-[1.04]
+          sm:group-even:group-hover:translate-x-3
+          sm:group-even:group-hover:translate-y-3
+          sm:group-even:group-hover:rotate-2
+
+          group-even:group-hover:-translate-x-3
+          group-even:group-hover:-translate-y-3
+          group-even:group-hover:-rotate-2"
         />
 
     </section>
